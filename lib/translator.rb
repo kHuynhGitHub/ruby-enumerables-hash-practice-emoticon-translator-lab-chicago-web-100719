858262251dbdddf783699b7128705db2ee8909ae
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-30 20:51:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(path)
   emoticons = YAML.load_file(path)
   list = {}
-  emoticons.each do |meaning,emojis|
+  emoticons.each do |meaning, emojis|
     if !list[:get_meaning]
       list[:get_meaning] = {emojis[1] => meaning}
     else list[:get_meaning][emojis[1]] = meaning
@@ -17,24 +17,18 @@ def load_library(path)
   list
 end
 
-
-
-
-def get_japanese_emoticon(path,emoji)
+def get_japanese_emoticon(path, emoji)
   list = load_library(path)
   if list[:get_emoticon][emoji]
     list[:get_emoticon][emoji]
-  else p 'Sorry, that emoticon was not found'
+  else p 'Sorry, that emoticon was not found.'
   end
 end
 
-
-
-
-def get_english_meaning(path,emoji)
+def get_english_meaning(path, emoji)
   list = load_library(path)
   if list[:get_meaning][emoji]
     list[:get_meaning][emoji]
-  else p "Sorry, that emoticon was not found"
+  else p "Sorry, that emoticon was not found."
   end
 end
